5350321fb86e4e0e90f7c5da1d562907071a05ca
SSsEnzo <30798984+SSsEnzo@users.noreply.github.com>
2017-12-09 15:27:34 +0900

    Add files via upload
    updated the distribution

--- js/main.js
+++ js/main.js
@@ -1,12 +1,45 @@
 $(document).ready(function(){
 
-  $(".description").hide();
 
-  $(".friends").click(function(){
-    $(this).next(".description").slideToggle();
+
+  $("#Steve-link").click(function(){
+    $(".description-container").load("Steve.html .description1");
+    return false;
   });
 
+  $("#Gunwoo-link").click(function(){
+    $(".description-container").load("Gunwoo.html .description1");
+    return false;
+  });
+
+  $("#Chanjung-link").click(function(){
+    $(".description-container").load("Chanjung.html .description1");
+    return false;
+  });
+
+  $("#Seungjae-link").click(function(){
+    $(".description-container").load("Seungjae.html .description1");
+    return false;
+  });
+
+  $("#Cheonho-link").click(function(){
+    $(".description-container").load("Cheonho.html .description1");
+    return false;
+  });
+
+  $("#Junho-link").click(function(){
+    $(".description-container").load("Junho.html .description1");
+    return false;
+  });
+
+  $("#Sunho-link").click(function(){
+    $(".description-container").load("Sunho.html .description1");
+    return false;
+  });
+  
 });
+
+  // $(".description").hide();
 
 //
 // var friendList = document.getElementById("Steve");
